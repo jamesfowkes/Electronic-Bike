8e08014667ec754cffb0750f8fe5bd6dbb2b9bce
James Fowkes <james.fowkes@zipher.co.uk>
2013-09-04 17:29:16 +0100

    Renamed identifiers in bike common header

--- Common/BikeCommon.h
+++ Common/BikeCommon.h
@@ -6,7 +6,7 @@ enum bike_protocol_enum
 	BP_NONE,
 	BP_PACKET_START,
 	BP_PING,
-	BP_INPUT_CONFIG,
+	BP_IO_CONFIG,
 	BP_READ_SENSORS,
 	BP_OUTPUT_CTRL,
 	BP_PACKET_END = 0xFE,
@@ -32,7 +32,7 @@ typedef enum bike_io_enum BIKE_IO_ENUM;
 #define BIKE_IO_CONFIG_FROM_BYTE(byte) ((IO_CONFIG_ENUM)((byte) & 0x0F))
 
 #define BIKE_IO_INDEX_TO_BYTE(idx) (((idx) << 4) & 0xF0)
-#define BIKE_IO_CONFIG_TO_BYTE(idx) ((idx) & 0x0F)
+#define BIKE_IO_CONFIG_TO_BYTE(cfg) ((cfg) & 0x0F)
 
 /* Output Setting*/
 
